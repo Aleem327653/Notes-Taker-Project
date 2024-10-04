08801b0c7b0331765a06ba83f4ed009db3171bb7
Aleem@327653 <mohdaleem327653@gmail.com>
2024-10-04 09:47:16 +0530

    Data Connected

--- src/main/java/com/servlet/DeleteServlet.java
+++ src/main/java/com/servlet/DeleteServlet.java
@@ -2,16 +2,17 @@ package com.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.entities.Notes;
 import com.helper.FactoryProvider;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 public class DeleteServlet extends HttpServlet {
